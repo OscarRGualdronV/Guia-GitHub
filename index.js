27389b7992ecb9dd61c1928fe2e6f88bf55b9901
oscar gualdron <oscarvilla09@outlook.es>
2024-04-14 12:36:18 -0500

    LaCajaPandora

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+
+    if (typeof numero !== 'number' || !Number.isInteger(numero)) {
+        throw new Error('El parámetro debe ser un número entero.');
     }
+
+    if (numero % 2 === 0) {
+        return numero.toString(2); 
+    } else {
+        return numero.toString(16); 
+    }
+    }
+
+    console.log(laCajaDePandora(4));
+console.log(laCajaDePandora(5)); 
